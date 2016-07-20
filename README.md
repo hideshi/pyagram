10297e72287b55a8fc49741a3ff66d405071aa7e
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2016-07-21 02:29:30 +0800

    Restructured the directory

---
+++ README.md
@@ -0,0 +1,18 @@
+Pyagram
+-------
+
+Python Finite State Machine Diagram Generator
+
+
+How to write source file
+
+::
+
+    See example 
+
+
+How to execute
+
+::
+
+    python3 pyagram.py -T {image type} -I {source file}
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'pyagram',
+    version = '1.0.0',
+    description = 'Pyagram: Python Finite State Machine Diagram Generator',
+    author = 'Hideshi Ogoshi',
+    author_email = 'hideshi.ogoshi@gmail.com',
+    url = 'https://github.com/hideshi',
+    packages = ['pyagram'],
+)
