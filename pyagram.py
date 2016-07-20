649ec5c0630697e6efc6d376c2366ae9e8e9a878
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2016-07-20 22:29:43 +0800

    Added syntactic analysis

--- pyagram.py
+++ pyagram.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import pprint
 import pyparsing as pp
 
 def lexical_analysis(src):
@@ -31,34 +32,58 @@ def lexical_analysis(src):
 
 def syntactic_analysis(src):
     prev = False
+    next_view_count = 0
+    count = 0
     d = {'graph': {}, 'views': {}, 'processes': {}}
     for elem in src:
+        #print(elem)
+        #print(prev)
         if elem[0] == '@' and elem[1] == '[' and elem[3] == ']':
             d['graph'][elem[2]] = ''
             prev = elem
 
         elif elem[0] == '#' and elem[1] == '[' and elem[3] == ']':
             d['views'][elem[2]] = {}
+            d['views'][elem[2]]['path'] = {}
+            d['views'][elem[2]]['next_view'] = {}
+            d['views'][elem[2]]['next_processes'] = {}
+            d['views'][elem[2]]['next_processes']['action'] = {}
+            d['views'][elem[2]]['next_processes']['process'] = {}
             prev = elem
+            next_view_count = 0
+            count = 0
 
         elif elem[0] == '$' and elem[1] == '[' and elem[3] == ']':
             d['processes'][elem[2]] = {}
+            d['processes'][elem[2]]['next_processes'] = {}
+            d['processes'][elem[2]]['next_processes']['action'] = {}
+            d['processes'][elem[2]]['next_processes']['process'] = {}
             prev = elem
+            count = 0
 
         elif prev and prev[0] == '@':
             d['graph'][prev[2]] = elem[0]
 
-        elif prev and prev[0] == '#':
-            d['views'][prev[2]] = elem[0]
+        elif prev and prev[0] == '#' and elem[0] != '-->' and elem[0].startswith('/'):
+            d['views'][prev[2]]['path'] = elem[0]
 
-        elif prev and prev[0] == '$':
-            d['processes'][prev[2]] = elem[0]
+        elif prev and prev[0] == '#' and elem[0] == '-->':
+            d['views'][prev[2]]['next_view'][next_view_count] = elem[1]
+            next_view_count = next_view_count + 1
 
-        #elif prev and prev[0] == '-->':
-        #    d['views'][prev[2]].append(elem[1])
+        elif prev and prev[0] == '#' and elem[0] != '==>':
+            d['views'][prev[2]]['next_processes']['action'][count] = elem[0]
 
-        #elif prev and prev[0] == '==>':
-        #    d['views'][prev[2]].append(elem[1])
+        elif prev and prev[0] == '#' and elem[0] == '==>':
+            d['views'][prev[2]]['next_processes']['process'][count] = elem[1]
+            count = count + 1
+
+        elif prev and prev[0] == '$' and elem[0] != '==>':
+            d['processes'][prev[2]]['next_processes']['action'][count] = elem[0]
+
+        elif prev and prev[0] == '$' and elem[0] == '==>':
+            d['processes'][prev[2]]['next_processes']['process'][count] = elem[1]
+            count = count + 1
 
     return d
 
@@ -66,14 +91,13 @@ def compile(filename):
     f = open(filename, 'r')
     src = []
     for line in f.readlines():
-        print(line)
-        res1 = lexical_analysis(line)
-        if (len(res1) == 1 and len(res1[0]) != 1) or len(res1) > 1:
+        l = line.replace('\n','')
+        if len(l) != 0:
+            res1 = lexical_analysis(l)
             src.append(res1)
-            print(res1)
-        print('--------------------------')
+    #print(src)
     res2 = syntactic_analysis(src)
-    print(res2)
+    pprint.pprint(res2)
 
 if __name__ == '__main__':
     compile(sys.argv[1])
